f9a72a4e99ece3c1c74a21ce49a34b108e7b2461
Ariel Coppes <ariel.coppes@gemserk.com>
2012-01-25 17:05:02 -0200

    added features.txt with some features in mind, modified the Test with more ideas

--- src/test/java/com/gemserk/tools/cantunethis/FirstTest.java
+++ src/test/java/com/gemserk/tools/cantunethis/FirstTest.java
@@ -4,6 +4,7 @@ import static org.junit.Assert.assertThat;
 
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
+import java.util.List;
 
 import org.hamcrest.core.IsEqual;
 import org.junit.Test;
@@ -12,11 +13,29 @@ import com.gemserk.commons.reflection.ReflectionUtils;
 
 public class FirstTest {
 
-	interface Property<T extends Object> {
+	static interface Property<T extends Object> {
 
 		T get();
 
 		void set(T value);
+
+	}
+
+	static class MockProperty<T> implements Property<T> {
+
+		private T t;
+
+		public MockProperty(T t) {
+			this.t = t;
+		}
+
+		public T get() {
+			return t;
+		}
+
+		public void set(T value) {
+			this.t = value;
+		}
 
 	}
 
@@ -40,10 +59,6 @@ public class FirstTest {
 
 	}
 
-	class Editor {
-
-	}
-	
 	static interface InternalField {
 
 		Object getValue(Object obj);
@@ -51,9 +66,9 @@ public class FirstTest {
 		void setValue(Object obj, Object value);
 
 	}
-	
+
 	// can be cached since it doesn't have state
-	
+
 	static class InternalFieldPublicImpl implements InternalField {
 
 		Field field;
@@ -79,7 +94,7 @@ public class FirstTest {
 		}
 
 	}
-	
+
 	static class InternalFieldMethodsReflectionImpl implements InternalField {
 
 		Method getterMethod;
@@ -107,7 +122,7 @@ public class FirstTest {
 		}
 
 	}
-	
+
 	static Method findMethod(Class clazz, String methodName) {
 		Method[] methods = clazz.getMethods();
 		for (Method method : methods)
@@ -115,7 +130,7 @@ public class FirstTest {
 				return method;
 		return null;
 	}
-	
+
 	static Field findField(Class clazz, String fieldName) {
 		try {
 			return clazz.getDeclaredField(fieldName);
@@ -125,56 +140,110 @@ public class FirstTest {
 			throw new RuntimeException(e);
 		}
 	}
-	
+
 	static InternalField internalFieldFromField(Class clazz, String fieldName) {
 		Field field = findField(clazz, "name");
 		field.setAccessible(true);
 		return new InternalFieldPublicImpl(field);
 	}
-	
+
 	static InternalField internalFieldFromMethods(Class clazz, String fieldName) {
 		Method setter = ReflectionUtils.getSetter(clazz, fieldName);
 		Method getter = ReflectionUtils.getGetter(clazz, fieldName);
 		return new InternalFieldMethodsReflectionImpl(getter, setter);
 	}
-	
+
 	static InternalField internalField(Class clazz, String fieldName) {
-		if (ReflectionUtils.getGetter(clazz, fieldName) != null &&
-			ReflectionUtils.getSetter(clazz, fieldName) != null) {
+		if (ReflectionUtils.getGetter(clazz, fieldName) != null && ReflectionUtils.getSetter(clazz, fieldName) != null) {
 			return internalFieldFromMethods(clazz, fieldName);
 		} else {
 			return internalFieldFromField(clazz, fieldName);
 		}
 	}
-	
+
 	static InternalField internalField(Object object, String fieldName) {
 		return internalField(object.getClass(), fieldName);
 	}
-	
+
 	static class MyObject {
-		
+
 		private String name;
-		
+
 		public void setName(String name) {
 			this.name = name;
 		}
-		
+
 		public String getName() {
 			return name;
 		}
-		
+
+	}
+
+	static class EditorProperty<T> {
+
+		String id;
+		Property<T> property;
+
+		public void setId(String id) {
+			this.id = id;
+		}
+
+		public String getId() {
+			return id;
+		}
+
+		public Property<T> get() {
+			return property;
+		}
+
+		public EditorProperty(String id, Property<T> property) {
+			this.id = id;
+			this.property = property;
+		}
+
+	}
+
+	// static class LimitedEditorProperty<T> extends EditorProperty<T> {
+	//
+	// private T min;
+	// private T max;
+	//
+	// public T getMin() {
+	// return min;
+	// }
+	//
+	// public T getMax() {
+	// return max;
+	// }
+	//
+	// public LimitedEditorProperty(String id, Property<T> property, T min, T max) {
+	// super(id, property);
+	// this.min = min;
+	// this.max = max;
+	// }
+	//
+	// }
+
+	static interface PropertyManager {
+
+		void register(String id, EditorProperty<?> property);
+
+		EditorProperty<?> get(String id);
+
+		List<String> listProperties();
+
 	}
 
 	@Test
 	public void test() {
 		MyObject myObject = new MyObject();
 		myObject.name = "Hello";
-		
+
 		Property<String> property = new ReflectionProperty<String>(myObject, internalField(myObject, "name"));
-		
+
 		assertThat(property.get(), IsEqual.equalTo("Hello"));
 		property.set("World");
-		assertThat(myObject.name,  IsEqual.equalTo("World"));
+		assertThat(myObject.name, IsEqual.equalTo("World"));
 	}
 
 }
